bf953894a8cd7e754e08fedf4e5a622516b4fe1d
Ramakanth V Prabhu <36660845+ramakanth0405@users.noreply.github.com>
2026-06-16 16:04:33 +0530

    Print HTML structure, title, footer, and article headlines

--- PG_S3_IoT/PG_S3_IoT_0f_web_scrape/PG_S3_IoT_0_web_scrape.py
+++ PG_S3_IoT/PG_S3_IoT_0f_web_scrape/PG_S3_IoT_0_web_scrape.py
@@ -3,4 +3,33 @@ import requests
 
 with open('PG_S3_IoT_0_sample_web.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
-    print(soup)
+
+    # To print full structure
+    print(soup) 
+
+    # Prints with structure
+    print(soup.prettify()) 
+
+    # To print div
+    match = soup.div 
+    print(match)
+
+    # To print title
+    print(soup.title)
+
+    # To print footer div
+    match_2 = soup.find('div', class_='footer') 
+    print(match_2)
+
+    # Extracting Articles
+    article = soup.find('div', class_='article')
+    print(article.h2.a.text) # To print article title
+    print(article.p.text) # To print article description
+
+    # Using a loop to print the articles
+    for article in soup.find_all('div', class_='article'):
+        headline=article.h2.a.text
+        print(headline)
+        summary=article.p.text
+        print(summary)
+
