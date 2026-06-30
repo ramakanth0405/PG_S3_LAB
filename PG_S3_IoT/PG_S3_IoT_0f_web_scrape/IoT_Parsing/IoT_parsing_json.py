1011d43d237076aa3260510fe0947a6f045bf196
Ramakanth V Prabhu <36660845+ramakanth0405@users.noreply.github.com>
2026-06-30 15:17:37 +0530

    feat(iot): add python file for json parsing

---
+++ PG_S3_IoT/PG_S3_IoT_0f_web_scrape/IoT_Parsing/IoT_parsing_json.py
@@ -0,0 +1,14 @@
+import json
+
+raw_json_payload = '{"sensor_id": "DHT22_01", "metrics": {"temp": 26.4, "humidity": 55}}'
+
+data = json.loads(raw_json_payload)
+
+print(f"Device_ID: {data['sensor_id']}")
+print(f"Current_Temperature: {data['metrics']['temp']} °C")
+
+processed_data = {
+    "gateway_id": "GW_MSc_01",
+    "status": "ONLINE",
+    "active_alerts": False
+}
